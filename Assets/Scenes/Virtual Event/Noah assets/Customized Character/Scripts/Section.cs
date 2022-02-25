caa13db484fb16da78ad6ddd3b26b4804085fde2
Wajana Piromnok <wajana.pir@spumail.net>
2022-02-25 21:07:10 +0700

    deploy-webgl-dev

---
+++ Assets/Scenes/Virtual Event/Noah assets/Customized Character/Scripts/Section.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Section : MonoBehaviour
+{
+    //public GameObject topLabel;
+    public GameObject deselectedTopIcon;
+    public GameObject selectedTopIcon;
+    public List<ButtonSection> buttonSections;
+
+    public void HandleState(bool activate)
+    {
+        //topLabel.SetActive(activate);
+        deselectedTopIcon.SetActive(!activate);
+        selectedTopIcon.SetActive(activate);
+    }
+}
---
+++ Assets/Scenes/Virtual Event/Noah assets/Dialogue System/Trigger chat/GetDress.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetDress : MonoBehaviour
+{
+    public void Dressing()
+    {
+        PlaygroundMaster.Instance.ShowAvatarGUI();
+    }
+}
