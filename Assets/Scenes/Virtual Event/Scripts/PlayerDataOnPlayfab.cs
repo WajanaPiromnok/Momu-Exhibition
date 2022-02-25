057622f509a1ba1cd4f1f671b5f89bb3e12fc348
Wajana Piromnok <wajana.pir@spumail.net>
2022-02-25 20:48:12 +0700

    discard master

--- Assets/Scenes/Virtual Event/Scripts/PlayerDataOnPlayfab.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class AvatarData {
-    public int top = -1;
-    public int body = -1;
-    public int bottom = -1;
-}
